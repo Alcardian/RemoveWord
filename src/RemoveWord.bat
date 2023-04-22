eab4814ab7aed9ce8d1fbb60bea42982bb83832e
Alcardian <alcardian@gmail.com>
2023-04-22 09:26:13 +0200

    Script remove word isn't hardcoded anymore. Input by user when running.

--- src/RemoveWord.bat
+++ src/RemoveWord.bat
@@ -1,9 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
+set /p word=Enter the word to remove from file names:
+
 for %%a in (*.*) do (
     set "filename=%%a"
-    set "newfilename=!filename:_loop=!"
+    set "newfilename=!filename:%word%=!"
     ren "%%a" "!newfilename!"
 )
 
